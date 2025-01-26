3f810991db45c283c9f02476a48b6035c53b44ea
John Doe <jkulcak444@gmail.com>
2025-01-26 13:40:02 -0600

    footer done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,15 +3,18 @@ import "./Navbar.css";
 import logo from "../../assets/logo.jpg";
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState("home");
+
   return (
     <div className="navbar">
       <img src={logo} height={100} alt="" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li><p>Home</p></li>
+        <li><p>About Me</p></li>
+        <li><p>Services</p></li>
+        <li><p>Portfolio</p></li>
+        <li><p>Contact</p></li>
       </ul>
       <div className="nav-connect">Connect With Me</div>
     </div>
