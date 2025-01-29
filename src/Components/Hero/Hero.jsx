8e87f324d0ab34f18f72e39ea81bc1e62cf80fbb
John Doe <jkulcak444@gmail.com>
2025-01-29 00:30:10 -0600

    installed particles package

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
     <div id="home" className="hero">
       <img src="https://github.com/jacobkulcak.png" height={200} alt="" />
       <h1>
-        <span>I'm Jacob Kulcak,</span> Based sigma male from Dallas, Texas
+        <span>I'm Jacob Kulcak,</span> Software Engineer based in Dallas, TX
       </h1>
-      <p>More info blah blah blah</p>
+      <p>I specialize in Full Stack development, Machine Learning, and Database Systems</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume">My resume</div>
