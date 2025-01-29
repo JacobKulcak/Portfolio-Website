ac1a77addba27343e8374a16e896089243e17c4b
John Doe <jkulcak444@gmail.com>
2025-01-28 22:50:22 -0600

    about and bio sections responsive to mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
+import {useRef} from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import "./Navbar.css";
 import logo from "../../assets/logo.jpg";
 import underline from "../../assets/nav_underline.png";
+import menu_open from '../../assets/menu_open.png';
+import menu_close from '../../assets/menu_close.png';
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right='0'
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right='-350px'
+  }
 
   return (
     <div className="navbar">
       <img src={logo} height={100} alt="" />
-      <img src="" alt="" />
-      <ul className="nav-menu">
+      <img src={menu_open} onClick={openMenu} height={40} className='nav-mob-open' />
+      <ul ref={menuRef} className="nav-menu">
+        <img src={menu_close} onClick={closeMenu} height={40} alt="" className="nav-mob-close" />
         <li>
           <AnchorLink className="anchor-link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
