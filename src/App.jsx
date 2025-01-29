e45a7fda1fa7ad2859015d5ed13ca9db9a3af2b5
John Doe <jkulcak444@gmail.com>
2025-01-29 11:10:05 -0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import Services from "./Components/Services/Services"
 import MyWork from "./Components/MyWork/MyWork";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import ParticlesComponent from "./Components/Particles/Particles";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Services/>
-      <MyWork/>
-      <Contact/>
-      <Footer/>
+      <ParticlesComponent id="particles" />
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <MyWork />
+      <Contact />
+      <Footer />
     </div>
   );
 };
